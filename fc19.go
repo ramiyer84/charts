36d03dc8f0f7bff57e12e77b2ec7cfe64584e06c
ramiyer84 <44645713+ramiyer84@users.noreply.github.com>
2025-10-08 10:03:56 +0100

    Create fc19.go

---
+++ fc19.go
@@ -0,0 +1,163 @@
+package fc19
+
+import (
+	"bufio"
+	"context"
+	"database/sql"
+	"errors"
+	"fmt"
+	"github.axa.com/axa-partners-clp/selmed-migration-tool/internal/inputfile"
+	"github.axa.com/axa-partners-clp/selmed-migration-tool/internal/logger"
+	"github.axa.com/axa-partners-clp/selmed-migration-tool/internal/util"
+	"strings"
+	"time"
+)
+
+type File struct {
+	*inputfile.SourceFile
+	Range *inputfile.Range
+}
+
+// Source table name: KJDTLED
+type FC19Record struct {
+	ApplicationNumber  string         // line[0] (IDGMDO)
+	ApplicationStatus  sql.NullString // line[1] (CEGMDO)
+	StatusCreationDate sql.NullTime   // line[2] (DDCHET)
+	RemoteTransmission sql.NullString // line[3] (CEGMET)
+	RecipientCodeType  sql.NullString // line[4] (CYGMDE)
+	ProtestorCodeType  sql.NullString // line[5] (CYGMCO)
+	DecisionTypeCode   sql.NullString // line[6] (CDGMDE)
+}
+
+func (fc File) AsyncProcessFile(ctx context.Context) {
+	fc.Process(ctx, fc.ProcessFile)
+}
+
+func (fc File) ProcessFile(ctx context.Context) error {
+	// If worker didn't validate, do it here once
+	if fc.Range != nil && !fc.Range.Loaded {
+		if err := fc.Range.Validate(ctx, fc.Db, fc.Tx, fc.BatchID); err != nil {
+			return fmt.Errorf("fc19: range validate failed: %w", err)
+		}
+	}
+	addedAt := time.Now().UTC()
+	id, err := fc.Db.AddFile(ctx, fc.Tx, fc.BatchID, fc.Filename, "FC19", addedAt)
+	if err != nil {
+		return err
+	}
+
+	r := bufio.NewScanner(fc.Reader)
+	// Skip the first line
+	r.Scan()
+
+	count := 0
+	added := 0
+	for r.Scan() {
+		count++
+		line := r.Text()
+		applicationNumber := line[:11]
+		if len(line) < 119 {
+			fc.Logger.Printf("line %d: application '%s' has incorrect line length %d", count, applicationNumber, len(line))
+			continue
+		}
+
+		if fc.Range.ApplicationNumberExists(applicationNumber) {
+			record, err := parseFC19Content(line)
+			if err != nil {
+				fc.Logger.Printf("cannot parse F19 line %d (%s): %v", count, line, err)
+				return err
+			}
+
+			err = addFC19Record(ctx, fc.Tx, id, record, addedAt)
+			if err != nil {
+				fc.Logger.Printf("cannot add F19 record to Database from line %d (%s): %v", count, line, err)
+				return err
+			}
+			added++
+		}
+	}
+
+	err = fc.Db.UpdateFileStatus(ctx, fc.Tx, "IMPORTED", id, time.Now().UTC())
+	if err != nil {
+		return err
+	}
+
+	fc.Logger.Printf("completed processing '%s' file, loaded %d records", fc.Filename, added)
+	return nil
+}
+
+func parseFC19Content(line string) (*FC19Record, error) {
+	data := []rune(line)
+
+	var err error
+	// Application Number
+	tmp := strings.TrimSpace(util.SubstringBeginning(data, 11))
+	record := FC19Record{
+		ApplicationNumber: tmp,
+	}
+	// Application Status
+	record.ApplicationStatus = util.ReadNullString(util.Substring(data, 12, 15))
+	// Status Creation Date
+	record.StatusCreationDate, err = util.ReadMacaoNullTimestamp(util.Substring(data, 16, 42))
+	if err != nil {
+		return nil, err
+	}
+	// Remote Transmission
+	record.RemoteTransmission = util.ReadNullString(util.Substring(data, 43, 44))
+	// Recipient Code Type
+	record.RecipientCodeType = util.ReadNullString(util.Substring(data, 45, 46))
+	// Protestor Code Type
+	record.ProtestorCodeType = util.ReadNullString(util.Substring(data, 47, 48))
+	// Decision Type Code
+	record.DecisionTypeCode = util.ReadNullString(util.SubstringEnd(data, 49))
+
+	return &record, nil
+}
+
+func CreateApplicationCreatedDateMap(l *logger.Logger, batchId string) (map[string]time.Time, error) {
+	key := fmt.Sprintf("SLMD_MACAOFC19_%s.txt", batchId)
+
+	//Open from S3 (io.ReadCloser)
+	body, err := s3client.Open(context.TODO()), key)
+	if err != nil {
+		// Translate S3 "not found" to our typed error
+		if isS3NotFound(err) {
+			return nil, &s3io.NotFoundError{Key: key}
+		}
+		return nil, err
+	}
+	defer body.Close()
+
+	result := make(map[string]time.Time)
+	r := bufio.NewScanner(body)
+	r.Scan() // skip header
+
+	return result, nil
+}
+
+func isS3NotFound(err error) bool {
+	var nsk *types.NoSuchKey
+	if errors.As(err, &nsk) {
+		return true
+	}
+	var apiErr smithy.APIError
+	if errors.As(err, &apiErr) {
+		return apiErr.ErrorCode() == "NoSuchKey"
+	}
+	return false
+}
+
+func addFC19Record(ctx context.Context, tx *sql.Tx, fileId uint, record *FC19Record, addedAt time.Time) error {
+	var id uint
+	err := tx.QueryRowContext(ctx, "INSERT INTO FC19_RECORDS (ID, FILE_ID, APPLICATION_NUMBER, APPLICATION_STATUS, "+
+		"STATUS_CREATION_DATE, REMOTE_TRANSMISSION, RECIPIENT_CODE_TYPE, PROTESTOR_CODE_TYPE, DECISION_TYPE_CODE, "+
+		"CREATED_AT) VALUES (NEXTVAL('FC19_SEQ'), $1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING ID",
+		fileId, record.ApplicationNumber, record.ApplicationStatus, record.StatusCreationDate, record.RemoteTransmission,
+		record.RecipientCodeType, record.ProtestorCodeType, record.DecisionTypeCode, addedAt).Scan(&id)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
